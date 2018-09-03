7a124253c9078de5dd9c2c706a5d0472b1d62bd0
Ahmad Izzan <aahmadizzan@gmail.com>
2018-09-03 22:41:31 +0700

    add main.js auth template

--- main.js
+++ main.js
@@ -1,11 +1,59 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+//connect to MongoDB
+var dbPath = 'mongodb://localhost:27017/test';
+var dbOptions = { useNewUrlParser: true };
 
-app.listen(3000, () => {
-	console.log('Connected to port 3000!');
+mongoose.connect(dbPath, dbOptions);
+
+var db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'DB Connection error:'));
+db.once('open', function () {
+	console.log('Connected to DB!')
+});
+
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+// listen on port 3000
+app.listen(3000, function () {
+  console.log('Express app listening on port 3000');
 });
