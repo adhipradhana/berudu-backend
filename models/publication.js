17144f767fb170d62a90621775d4d30f9f920935
yusufrahmatp <yusufrahmatp@gmail.com>
2018-09-12 10:50:59 +0700

    Add paginate for articles select with limit 10 per page

--- models/publication.js
+++ models/publication.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate')
 var Article = require('./article')
 
 var PublicationSchema =  new mongoose.Schema({
@@ -41,14 +42,22 @@ PublicationSchema.methods.removeSubscriber = function removeSubscriber (userID) 
   } 
 }
 
-PublicationSchema.methods.findPublicationArticle = function findPublicationArticle (callback) {
-	Article.find({publicationID: this.publicationID}, function (err, articles) {
+PublicationSchema.methods.findPublicationArticle = function findPublicationArticle (page, callback) {
+	Article.paginate(Article.find({publicationID: this.publicationID}), {page: page, limit: 10}, function(err, articles) {
 		if (err) {
 			return callback(err);
 		}
 
-		return callback(null, articles);
+		return callback(null, articles.docs);
 	});
+
+	// Article.find({publicationID: this.publicationID}, function (err, articles) {
+	// 	if (err) {
+	// 		return callback(err);
+	// 	}
+
+	// 	return callback(null, articles);
+	// });
 }
 
 var Publication = mongoose.model('Publication', PublicationSchema);
